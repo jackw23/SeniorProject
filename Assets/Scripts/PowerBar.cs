c75e121a825cc898382cd94d6f818aef23d84939
Julia McKay <juliamckay@ufl.edu>
2022-09-22 17:31:50 -0400

    Added functionality to swap active element

--- Assets/Scripts/PowerBar.cs
+++ Assets/Scripts/PowerBar.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerBar : MonoBehaviour
 {
+    //Bar Elements
+    [SerializeField] public Image healthFill;
+    [SerializeField] public Image powerFill;
+    [SerializeField] public GameObject fire;
+    [SerializeField] public GameObject water;
+    [SerializeField] public GameObject earth;
+    [SerializeField] public GameObject air;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fire.SetActive(true);
+        water.SetActive(false);
+        earth.SetActive(false);
+        air.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            fire.SetActive(true);
+            water.SetActive(false);
+            earth.SetActive(false);
+            air.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            fire.SetActive(false);
+            water.SetActive(true);
+            earth.SetActive(false);
+            air.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            fire.SetActive(false);
+            water.SetActive(false);
+            earth.SetActive(true);
+            air.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            fire.SetActive(false);
+            water.SetActive(false);
+            earth.SetActive(false);
+            air.SetActive(true);
+        }
     }
 }
