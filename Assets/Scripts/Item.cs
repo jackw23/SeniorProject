ea28dd80775aad45bef3b79eb5f028753211d3ce
Jack-w <61335184+jjjjjj4245@users.noreply.github.com>
2022-09-28 20:39:50 -0400

    early progress new tutorial

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 // SOURCE CODE HEAVILY INSPIRED BY: https://github.com/Brackeys/RPG-Tutorial/blob/master/Finished%20Project/Assets/Scripts/Items/Item.cs
 // USE https://www.youtube.com/watch?v=AoD_F1fSFFg for clicking to pick up item bc rn you need it to showcase that it works
-[CreateAssetMenu (fileName = "New Item", menuName = "Inventory/Item")]
+[CreateAssetMenu (fileName = "New Item", menuName = "Item/Create New Item")]
 public class Item : ScriptableObject
 {
-    public int number = -1;
-    public new string name = "new item"; // using new keyword hides the object.name??
-    public Sprite icon = null;
-    public bool showInInventory = false;
+    public int id;
+    public int value;
+    public string itemName = "new item"; // using new keyword hides the object.name??
+    public Sprite icon;
 
     public virtual void Use()
     {
