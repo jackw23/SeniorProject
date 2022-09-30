a4c1c9ac84958894150ffede9d06f6a5626e76d5
JofaTech <80646647+JofaTech@users.noreply.github.com>
2022-09-29 21:40:19 -0400

    Fixed background parallax script

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -8,7 +8,7 @@ public class ScrollingBackground : MonoBehaviour
     [SerializeField] Transform _first, _second;
     [SerializeField] private float _scrollSpeed, _zOffset;
 
-    private float _horizontalDifference = 37.46f, _minimumDistance = -37.46f;
+    private float _horizontalDifference = 37.46f, _minimumDistance = 37.46f;
 
     private Vector3 newPosition;
 
@@ -33,15 +33,15 @@ public class ScrollingBackground : MonoBehaviour
 
     void Wrap()
     {
-        if (_first.localPosition.x < _minimumDistance)
+        if (_first.localPosition.x > _minimumDistance)
         {
-            newPosition = new Vector3(_second.position.x * _horizontalDifference * 2, _second.position.y, _zOffset);
+            newPosition = new Vector3(_second.position.x - _horizontalDifference, _second.position.y, _zOffset);
             _first.position = newPosition;
         }
 
-        if (_second.localPosition.x < _minimumDistance)
+        if (_second.localPosition.x > _minimumDistance)
         {
-            newPosition = new Vector3(_first.position.x * _horizontalDifference * 2, _first.position.y, _zOffset);
+            newPosition = new Vector3(_first.position.x - _horizontalDifference, _first.position.y, _zOffset);
             _second.position = newPosition;
         }
     }
