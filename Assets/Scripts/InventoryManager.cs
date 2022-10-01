2316e49f8cdfb38021d33d75f0e5ded379035f13
Jack-w <61335184+jjjjjj4245@users.noreply.github.com>
2022-10-01 17:07:42 -0400

    can show item in inventory correctly after picking up

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static InventoryManager Instance;
     public List<Item> Items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItems;
     private void Awake()
     {
         Instance = this;
@@ -20,5 +25,38 @@ public class InventoryManager : MonoBehaviour
     public void Remove(Item item)
     {
         Items.Remove(item);
+    }
+
+    public void ListItem()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItems, ItemContent);
+
+            //Debug.Log("Picking up " + Item.name);
+
+
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            Debug.Log(itemName);
+            Debug.Log(itemIcon);
+
+            //var itemName = obj;
+            //var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+
+            //GameObject ChildGameObject1 = ParentGameObject.transform.GetChild(0).gameObject;
+            //obj.transform.GetChild(0) = item.itemName;
+            //obj.sprite = item.icon;
+
+        }
     }
 }
