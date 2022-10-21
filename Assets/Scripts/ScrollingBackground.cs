d8b40cb44feb4e489bbf879bfdd8dc69dea5ba2d
JofaTech <80646647+JofaTech@users.noreply.github.com>
2022-10-20 20:21:55 -0400

    Added assets to main scene including moveable player and following camera script

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class ScrollingBackground : MonoBehaviour
 {
     // Create references to two background sprites and float variable for movement speed
-    [SerializeField] Transform _first, _second;
+    [SerializeField] Transform _first, _second, _third;
     [SerializeField] private float _scrollSpeed, _zOffset;
 
-    private float _horizontalDifference = 37.46f, _minimumDistance = 37.46f;
+    private float _horizontalDifference = 37.63f, _minimumDistance = -37.63f;
 
     private Vector3 newPosition;
 
@@ -29,20 +29,27 @@ public class ScrollingBackground : MonoBehaviour
     {
         _first.position = new Vector3(_first.position.x + Time.deltaTime * _scrollSpeed, _first.position.y, _zOffset);
         _second.position = new Vector3(_second.position.x + Time.deltaTime * _scrollSpeed, _second.position.y, _zOffset);
+        _third.position = new Vector3(_third.position.x + Time.deltaTime * _scrollSpeed, _third.position.y, _zOffset);
     }
 
     void Wrap()
     {
-        if (_first.localPosition.x > _minimumDistance)
+        if (_first.localPosition.x < _minimumDistance)
         {
             newPosition = new Vector3(_second.position.x - _horizontalDifference, _second.position.y, _zOffset);
             _first.position = newPosition;
         }
 
-        if (_second.localPosition.x > _minimumDistance)
+        if (_second.localPosition.x < _minimumDistance)
         {
             newPosition = new Vector3(_first.position.x - _horizontalDifference, _first.position.y, _zOffset);
             _second.position = newPosition;
+        }
+
+        if (_second.localPosition.x < _minimumDistance)
+        {
+            newPosition = new Vector3(_first.position.x - _horizontalDifference, _first.position.y, _zOffset);
+            _third.position = newPosition;
         }
     }
 
