48493059c02ace8f8ef8096c98fcc04aa88db712
JofaTech <80646647+JofaTech@users.noreply.github.com>
2022-10-25 19:46:22 -0400

    Clamped camera, added assets, fixed clouds

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class ScrollingBackground : MonoBehaviour
 {
     // Create references to two background sprites and float variable for movement speed
-    [SerializeField] Transform _first, _second, _third;
+    [SerializeField] Transform _first, _second, _third, _fourth;
     [SerializeField] private float _scrollSpeed, _zOffset;
 
-    private float _horizontalDifference = 37.63f, _minimumDistance = -37.63f;
+    private float _horizontalDifference = 35.00f, _maximumDistance = 65.00f;
 
     private Vector3 newPosition;
 
@@ -30,26 +30,33 @@ public class ScrollingBackground : MonoBehaviour
         _first.position = new Vector3(_first.position.x + Time.deltaTime * _scrollSpeed, _first.position.y, _zOffset);
         _second.position = new Vector3(_second.position.x + Time.deltaTime * _scrollSpeed, _second.position.y, _zOffset);
         _third.position = new Vector3(_third.position.x + Time.deltaTime * _scrollSpeed, _third.position.y, _zOffset);
+        _fourth.position = new Vector3(_fourth.position.x + Time.deltaTime * _scrollSpeed, _fourth.position.y, _zOffset);
     }
 
     void Wrap()
     {
-        if (_first.localPosition.x < _minimumDistance)
+        if (_first.localPosition.x > _maximumDistance)
         {
             newPosition = new Vector3(_second.position.x - _horizontalDifference, _second.position.y, _zOffset);
             _first.position = newPosition;
         }
 
-        if (_second.localPosition.x < _minimumDistance)
+        if (_second.localPosition.x > _maximumDistance)
         {
-            newPosition = new Vector3(_first.position.x - _horizontalDifference, _first.position.y, _zOffset);
+            newPosition = new Vector3(_third.position.x - _horizontalDifference, _third.position.y, _zOffset);
             _second.position = newPosition;
         }
 
-        if (_second.localPosition.x < _minimumDistance)
+        if (_third.localPosition.x > _maximumDistance)
+        {
+            newPosition = new Vector3(_fourth.position.x - _horizontalDifference, _fourth.position.y, _zOffset);
+            _third.position = newPosition;
+        }
+
+        if (_fourth.localPosition.x > _maximumDistance)
         {
             newPosition = new Vector3(_first.position.x - _horizontalDifference, _first.position.y, _zOffset);
-            _third.position = newPosition;
+            _fourth.position = newPosition;
         }
     }
 
