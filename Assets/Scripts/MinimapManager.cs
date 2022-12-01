2c7233384e3aa8331d470e18b5796ed0c4f9c1e7
Julia McKay <juliamckay@ufl.edu>
2022-12-01 18:20:43 -0500

    Fixed small bugs with UI in Well and Graveyard scene, added minimap unlock

---
+++ Assets/Scripts/MinimapManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MinimapManager : MonoBehaviour
+{
+    static MinimapManager instance;
+    [SerializeField] public List<GameObject> areas;
+    [SerializeField] private List<int> visitedAreas;
+
+    private void Awake()
+    {
+        //Create singleton so progress will be preserved across scenes
+        SetUpSingleton();
+        //Hide all undiscovered areas
+        for (int i = 0; i < areas.Count; i++)
+        {
+            areas[i].SetActive(true);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //get current scene number
+        int sceneNum = scene.buildIndex;
+        //add to visitedAreas if not present
+        if (!visitedAreas.Contains(sceneNum))
+        {
+            visitedAreas.Add(sceneNum);
+        }
+
+        //re-attach areas
+        areas[0] = GameObject.Find("Graveyard");
+        areas[1] = GameObject.Find("Well");
+        areas[2] = GameObject.Find("Underground (1)");
+
+        UnHideVisitedScenes();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void UnHideVisitedScenes()
+    {
+        for (int i = 0; i < areas.Count; i++)
+        {
+            if (visitedAreas.Contains(i+1))
+            {
+                areas[i].SetActive(false);
+            }
+        }
+    }
+}
